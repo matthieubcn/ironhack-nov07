5a4d09e478963c186dea4afc01954c2b19d5952f
Diego Zito <diegozito.m@gmail.com>
2022-11-14 16:10:21 +0100

    operators - excerciser

--- section-3-js/06-operators.js
+++ section-3-js/06-operators.js
@@ -23,40 +23,200 @@ console.log(add);
 // Si miramos el ejemplo en la linea 6, el operador de asignacion "=" es lo que usamos para apuntar/asignar la operacion matematica con la variable "add"...
 
 console.log("Assignment Operators");
+// console.table([
+//   {
+//     operator: "=",
+//     name: "Assignment operator",
+//     example: "a = 7; // 7",
+//   },
+//   {
+//     operator: "+=",
+//     name: "Addition operator",
+//     example: "a += 5; // a = a + 5",
+//   },
+//   {
+//     operator: "-=",
+//     name: "Subtraction Assignment",
+//     example: "a -= 2; // a = a - 2",
+//   },
+//   {
+//     operator: "*=",
+//     name: "Multiplication Assignment",
+//     example: "a *= 3; // a = a * 3",
+//   },
+//   {
+//     operator: "/=",
+//     name: "Division Assignment",
+//     example: "a /= 2; // a = a / 2",
+//   },
+//   {
+//     operator: "%=",
+//     name: "Remainder Assignment",
+//     example: "a %= 2; // a = a % 2",
+//   },
+//   {
+//     operator: "**=",
+//     name: "Exponentiation Assignment",
+//     example: "a **= 2; // a = a**2",
+//   },
+// ]);
+
+console.log("Arithemical Operators");
+// console.table([
+//   {
+//     operator: "+",
+//     name: "Add",
+//     example: "2+2 // 4",
+//   },
+//   {
+//     operator: "-",
+//     name: "Substract",
+//     example: "4-2 // 2",
+//   },
+//   {
+//     operator: "*",
+//     name: "Multiplication",
+//     example: "4 * 4 // 16",
+//   },
+//   {
+//     operator: "/",
+//     name: "Division",
+//     example: "3 /3 // 1",
+//   },
+//   {
+//     operator: "++",
+//     name: "Increment (increments by 1)",
+//     example: "4++ // 5",
+//   },
+//   {
+//     operator: "--",
+//     name: "Decrement (decrements by 1)",
+//     example: "4-- // 3",
+//   },
+//   {
+//     operator: "**",
+//     name: "Exponentiation (Power)",
+//     example: "2**2 // 8",
+//   },
+// ]);
+
+// JS Comparison Operators
+// Operadores de comparasion, comparan dos tipos de valores o mas+ y te retornan un valor booleanico.
+// Ejemplo
+
+let number1 = 3;
+let number2 = 2;
+
+console.log("Comparing these 2 numbers");
+console.log(number1 > number2);
+
+console.log("Comparison Operators");
+// console.table([
+//   {
+//     operator: "==",
+//     description: "Equal to: returns true if the operands are equal",
+//     example: "x == y",
+//   },
+//   {
+//     operator: "!=",
+//     description: "Not equal to: returns true if the operands are not equal",
+//     example: "x != y",
+//   },
+//   {
+//     operator: "===",
+//     description:
+//       "Strict equal to: true if the operands are equal and of the same type",
+//     example: "x === y",
+//   },
+//   {
+//     operator: "!==",
+//     description:
+//       "Strict not equal to: true if the operands are equal but of different type or not equal at all",
+//     example: "x !== y",
+//   },
+//   {
+//     operator: ">",
+//     description:
+//       "Greater than: true if left operand is greater than the right operand",
+//     example: "x > y",
+//   },
+//   {
+//     operator: ">=",
+//     description:
+//       "Greater than or equal to: true if left operand is greater than or equal to the right operand",
+//     example: "x >= y",
+//   },
+//   {
+//     operator: "<",
+//     description:
+//       "Less than: true if the left operand is less than the right operand",
+//     example: "x < y",
+//   },
+//   {
+//     operator: "<=",
+//     description:
+//       "Less than or equal to: true if the left operand is less than or equal to the right operand",
+//     example: "x <= y",
+//   },
+// ]);
+// console.log(8 == 8);
+// console.log(8 != 4);
+// console.log(8 === "8");
+// console.log(8 !== "8");
+// console.log(4 > 2);
+// console.log(4 < 2);
+// console.log(2 >= 2);
+// console.log(5 <= 9);
+
+// JS Logical Operators
+// Ejecutan operaciones logicas, que nos brindan ayuda para segmentar ciertas operaciones.
+
+const numberA = 5;
+const numberB = 3;
+
+console.log(
+  `The comparison of these numbers is: ${numberA < 6 && numberB < 5}`
+);
+
+console.log("Logical Operators");
 console.table([
   {
-    operator: "=",
-    name: "Assignment operator",
-    example: "a = 7; // 7",
+    operator: "&&",
+    description:
+      "Logical AND: true if both the operands are true, else returns false",
+    example: "x && y",
   },
   {
-    operator: "+=",
-    name: "Addition operator",
-    example: "a += 5; // a = a + 5",
+    operator: "||",
+    description:
+      "Logical OR: true if either of the operands is true; returns false if both are false",
+    example: "x || y",
   },
   {
-    operator: "-=",
-    name: "Subtraction Assignment",
-    example: "a -= 2; // a = a - 2",
-  },
-  {
-    operator: "*=",
-    name: "Multiplication Assignment",
-    example: "a *= 3; // a = a * 3",
-  },
-  {
-    operator: "/=",
-    name: "Division Assignment",
-    example: "a /= 2; // a = a / 2",
-  },
-  {
-    operator: "%=",
-    name: "Remainder Assignment",
-    example: "a %= 2; // a = a % 2",
-  },
-  {
-    operator: "**=",
-    name: "Exponentiation Assignment",
-    example: "a **= 2; // a = a**2",
+    operator: "!",
+    description: "Logical NOT: true if the operand is false and vice-versa.	",
+    example: "!x",
   },
 ]);
+
+// Logical AND
+console.log(true && true);
+console.log(true && false);
+
+// Logical OR
+console.log(true || false);
+console.log(false || false);
+
+// Logical NOT
+console.log(!true);
+
+// Strings + operators...
+// En js podemos usar algunos operadores para otras operaciones que no corresponden matematica y logica contextual...
+console.log("Wu Tang Clan" + " is for the children");
+
+//function to add two numbers
+function addTwoNumbers(number1, number2) {
+  return number1 + number2;
+}
+
+console.log(addTwoNumbers(4, 8));
