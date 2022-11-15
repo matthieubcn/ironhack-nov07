cbe4c2d851fd0f7798f8728d6995e090d00abf93
Diego Zito <diegozito.m@gmail.com>
2022-11-15 13:16:09 +0100

    functions intro - with excercised

--- section-3-js/08-functions.js
+++ section-3-js/08-functions.js
@@ -167,3 +167,7 @@ console.log(resultOfSum);
 // EJERCICIO #1
 // Crea una función que reciba tres argumentos: una string name, un numero age y un objeto livesIn. Ten en cuenta que el objeto livesIn será un objeto que contiene dos keys: livesInCity y livesInTown. Las dos tendrán un valor booleánico y siempre será una falsa y la otra verdadera.
 // Crea una función tipo flecha que retorne una string parecida a "Diego tiene 30 años y vive en la ciudad".Según la key que sea true en el argumento livesIn vamos a retornar vive en la ciudad o vive en un pueblo.
+
+//console.log(miFuncion("Jarko", 25, {livesInCity: true, livesInTown: false}));
+//console.log(miFuncion("Martina", 42, {livesInCity: false, livesInTown: true}));
+//console.log(miFuncion("Laia", 78, { livesInCity: false, livesInTown: true }));
