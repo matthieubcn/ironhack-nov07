81bc6c16106b06b69d9f53964a87ca690c13c67d
Diego Zito <diegozito.m@gmail.com>
2022-11-15 13:09:01 +0100

    functions intro - with excercised

--- section-3-js/08-functions.js
+++ section-3-js/08-functions.js
@@ -163,3 +163,6 @@ let sum = (a, b) => {
 };
 let resultOfSum = sum(5, 89);
 console.log(resultOfSum);
+
+// EJERCICIO #1
+// Crea una función que reciba tres argumentos: una string name, un numero age y un objeto livesIn. Ten en cuenta que el objeto livesIn será un objeto que contiene dos keys: livesInCity y livesInTown. Las dos tendrán un valor booleánico y siempre será una falsa y la otra verdadera. Crea una función tipo flecha que retorne una string parecida a "Diego tiene 30 años y vive en la ciudad". Según la key que sea true en el argumento livesIn vamos a retornar vive en la ciudad o vive en un pueblo.
