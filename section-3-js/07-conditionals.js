f230f7864585f1b67962be26cd086c1dad2c4d05
Diego Zito <diegozito.m@gmail.com>
2022-11-15 12:16:12 +0100

    conditionals - part 2 - updatedd

--- section-3-js/07-conditionals.js
+++ section-3-js/07-conditionals.js
@@ -245,20 +245,42 @@ console.log("SWITCH - Simple Calculator");
 console.log("-------------------");
 
 console.log("SWITCH - Multiple Cases");
+// // Ejemplo complejo - SWITCH - COUNTRY CONTINET FINDER - CASOS MULTIPLES
+// const countryOfOrigin = prompt("What country where you born?");
+// switch (countryOfOrigin) {
+//   case "Venezuela":
+//   case "Colombia":
+//   case "Ecuador":
+//   case "Brazil":
+//     alert(`Cool, ${countryOfOrigin} is located in South America`);
+//     console.log(`Cool, ${countryOfOrigin} is located in South America`);
+//     break;
+//   case "Spain":
+//   case "France":
+//   case "Portugal":
+//   case "Andorra":
+//     alert(`Cool, ${countryOfOrigin} is locaded in Europe`);
+//     console.log(`Cool, ${countryOfOrigin} is locaded in Europe`);
+//     break;
+//   default:
+//     alert("you are not from this planet! go Invade somewhere else...");
+//     console.log("you are not from this planet! go Invade somewhere else...");
+// }
+
 // Ejemplo complejo - SWITCH - COUNTRY CONTINET FINDER - CASOS MULTIPLES
 const countryOfOrigin = prompt("What country where you born?");
-switch (countryOfOrigin) {
-  case "Venezuela":
-  case "Colombia":
-  case "Ecuador":
-  case "Brazil":
+switch (countryOfOrigin.toLowerCase()) {
+  case "venezuela":
+  case "colombia":
+  case "ecuador":
+  case "brazil":
     alert(`Cool, ${countryOfOrigin} is located in South America`);
     console.log(`Cool, ${countryOfOrigin} is located in South America`);
     break;
-  case "Spain":
-  case "France":
-  case "Portugal":
-  case "Andorra":
+  case "spain":
+  case "france":
+  case "portugal":
+  case "andorra":
     alert(`Cool, ${countryOfOrigin} is locaded in Europe`);
     console.log(`Cool, ${countryOfOrigin} is locaded in Europe`);
     break;
