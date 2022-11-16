7ed46db77f35eb4acd0093d5d3ea3e2417159802
Diego Zito <diegozito.m@gmail.com>
2022-11-16 13:45:16 +0100

    dom manipulation - start

---
+++ section-3-js/11-dom-manipulation.js
@@ -0,0 +1,51 @@
+// DOM MANIPULATION
+
+// En el desarrollo de sitios web, DOM son las siglas de Document Object Model. Se trata de una interfaz de programación que nos permite crear, modificar o eliminar elementos de un documento web. La manipulación del DOM es cuando se utiliza JavaScript para añadir, eliminar y modificar elementos de un sitio web.
+
+// ¿Qué es el DOM?
+// El DOM significa Document Object Model (Modelo de Objetos del Documento). Puede entenderse simplemente como un árbol de nodos creado por el navegador. Cada uno de estos nodos tiene sus propias propiedades y métodos que pueden ser manipulados usando JavaScript.
+
+// La capacidad de manipular el DOM es una de las habilidades más únicas y útiles de JavaScript.
+
+// La imagen siguiente ofrece una representación visual del aspecto del árbol DOM.
+
+// El Documento
+// La interfaz Document representa cualquier página web cargada en el navegador y sirve como punto de entrada al contenido de la página web, que es el árbol DOM (Document Object Model).
+// El árbol DOM incluye elementos como <body> y <table>, entre muchos otros. Proporciona funcionalidad globalmente al documento, como obtener la URL de la página y crear nuevos elementos en el documento.
+
+console.log(window);
+console.log(document);
+console.log(document.title);
+console.log(document.URL);
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+
+// Cómo seleccionar elementos en el DOM
+// Para poder manipular un elemento en el DOM, tienes que seleccionar ese elemento en particular. Por suerte para nosotros tenemos 4 formas principales de seleccionar elementos.
+
+//Una vez que hayas seleccionado un elemento, puede añadir estilos al elemento, manipular sus atributos y recorrer los elementos padre e hijo.
+
+// 1. getElementById Method
+// // La forma más común de acceder a un elemento HTML es utilizar el id del elemento.
+//El id distingue entre mayúsculas y minúsculas. Por ejemplo, el "dom-manipulation" y el "Dom-manipulation" son ids totalmente diferentes.
+console.log(document.getElementById("wutang"));
+
+// 2. getElementsByClassName() Method
+// Este método devuelve una colección de todos los elementos del documento con el nombre de clase especificado.
+// Por ejemplo, nuestra página HTML de abajo contiene 5 elementos con class="wuTang".
+console.log(document.getElementsByClassName("wuTang"));
+console.log(document.getElementsByClassName("wuTang")[1]);
+
+// 3. getElementsByTagName() Method
+// Este método acepta un nombre de etiqueta y devuelve todos los elementos del nombre de etiqueta especificado en el orden en que aparecen en el documento.
+console.log(document.getElementsByTagName("h5"));
+console.log(document.getElementsByTagName("h5")[0]);
+
+// 4. querySelector() Method
+// Devuelve el primer valor que coincida con el selector que se le da. Este método puede aceptar todos los selectores de estilo CSS, lo que le permite seleccionar por etiqueta, clase o ID.
+console.log(document.querySelector("#query-selector"));
+console.log(document.querySelector(".hipHop"));
+console.log(document.querySelector("h5"));
+console.log(document.querySelector("body"));
+console.log(document.querySelector("#query-selector .hipHop .myLove"));
