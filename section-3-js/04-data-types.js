b85d117beff3b1baec2a49ab910dc19b1d2dde89
Diego Zito <diegozito.m@gmail.com>
2022-11-21 12:48:31 +0100

    fetch api - class explanation

--- section-3-js/04-data-types.js
+++ section-3-js/04-data-types.js
@@ -112,32 +112,32 @@ console.log("My Bio");
 
 // EXAMPLE 2
 // ARRAY OF OBJECTS
-// console.table([
-//   {
-//     movie: "Wolf of Wallstreet",
-//     genre: ["Drama", "Action", "Comedy"],
-//     director: ["Martin Scorsese"],
-//     location: "New York City, NY",
-//     actors: ["Jonah Hill", "Margo Robbie", "Leo Di Caprio"],
-//     year: 2010,
-//   },
-//   {
-//     movie: "Fight Club",
-//     genre: ["Drama", "Noir", "Comedy", "Action"],
-//     director: ["David Fincher"],
-//     location: "Los Angeles, California",
-//     actors: ["Bradd Pitt", "Edward Norton", "Helena Bonham Carter"],
-//     year: 1999,
-//   },
-//   {
-//     movie: "The Neon Demon",
-//     genre: ["Aleix", "Thriller", "Drama"],
-//     director: ["Nicolas Winding Refn"],
-//     location: "Copenhagen, Denmark",
-//     actors: ["Elle Fanning", "Jenna Malone", "Keanu Reeves"],
-//     year: 2016,
-//   },
-// ]);
+console.table([
+  {
+    movie: "Wolf of Wallstreet",
+    genre: ["Drama", "Action", "Comedy"],
+    director: ["Martin Scorsese"],
+    location: "New York City, NY",
+    actors: ["Jonah Hill", "Margo Robbie", "Leo Di Caprio"],
+    year: 2010,
+  },
+  {
+    movie: "Fight Club",
+    genre: ["Drama", "Noir", "Comedy", "Action"],
+    director: ["David Fincher"],
+    location: "Los Angeles, California",
+    actors: ["Bradd Pitt", "Edward Norton", "Helena Bonham Carter"],
+    year: 1999,
+  },
+  {
+    movie: "The Neon Demon",
+    genre: ["Aleix", "Thriller", "Drama"],
+    director: ["Nicolas Winding Refn"],
+    location: "Copenhagen, Denmark",
+    actors: ["Elle Fanning", "Jenna Malone", "Keanu Reeves"],
+    year: 2016,
+  },
+]);
 
 // Using the same object but storing it within a variable:
 const movies = [
