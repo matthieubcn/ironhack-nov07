9640a93f5b9db3e5dae7c44c8ef97330cf21dc23
Diego Zito <diegozito.m@gmail.com>
2022-11-29 12:24:45 +0100

    conditional rendering directives - day 002 - vue - class excercises

--- section-04-vue/my-first-vue-project/src/components/vueBasics/5-ConditionalRendering.vue
+++ section-04-vue/my-first-vue-project/src/components/vueBasics/5-ConditionalRendering.vue
@@ -62,6 +62,8 @@
     <h2 v-show="showElement">Usando un v-show directive</h2>
     <h2 v-if="showElement">Usando un v-if directive</h2>
   </div>
+
+  <div id="class-excercises"></div>
 </template>
 
 <script setup>
@@ -78,6 +80,17 @@ const display = ref(true);
 
 //Ejemplos de conditional rendering part 5
 const showElement = ref(false);
+
+// CLASS EXCERCISE EXPLANATION
+// Ejercicios Conditional Rendering
+
+// Creamos la estructura básica de un componente de vue en la que vamos a mostrar el resultado de los ejercicios. Recordad importar el componente donde sea necesario para poder ver el avance de vuestros ejercicios.
+
+// Ej.1: Creamos dos <h1> diferentes, uno que diga "I'm showing up beacuse our reactive data is true!" y el otro que diga "I'm the option anytime our reactive data cannot be read as true". Como vemos, los dos dependerán de una variable reactiva en vuestro script y, según si esta es true or false, enseñaréis uno u otro.
+
+// Ej.2: Creamos cuatro <p> tags. Los cuatro se renderizarán condicionalmente mirando a una misma variable que llamaremos "language". Esta variable podrá tener tres valores distintos: "HTML", "CSS" o "JS". En nuestro template deberemos leer dicha variable y enseñar un <p> u otro según su valor. Cada uno de los <p> tags a su vez contendrá el texto "Today we are learing" seguido del valor de la variable que imprimiréis en pantalla usando la moustache syntax. El último <p> tag se encargará de aparecer cuando el valor de "language" no sea ninguno de los tres mencionado anteriormente, y contendrá el texto "Today there's no class!".
+
+// Ej.3***: Creamos una variable reactiva que contenga un objeto con vuestro nombre (string), vuestra edad (number) y una key hasCodedBefore (boolean) la cual será true or false dependiendo de si antes del bootcamp habíais picado código o no. Después creamos otra variable reactiva booleánica que podéis llamar show, por ejemplo. Según si show está en true or false, en vuestro template deberíais enseñar o no una pequeña biografía. Esta biografía, a su vez, constará primero de vuestro nombre como título. Después tendrá un <p> que se renderizará de manera condicional. En caso de que vuestra edad sea superior a 18 enseñaremos la frase "I am an adult! :)"; en caso contrario, enseñaremos "I'm still a minor :(". Finalmente, según si hasCodedBefore es true or false también renderizaréis "I am still a newbie to code but I'm learning fast" o "This is not my first time at the javascript rodeo".
 </script>
 
 <style scoped>
