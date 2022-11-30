bf3daa64385e497c16d27334ac22ef7c47047c3c
Diego Zito <diegozito.m@gmail.com>
2022-11-30 15:39:34 +0100

    vue day 003 - props class intro

---
+++ section-04-vue/my-first-vue-project/src/components/vueBasics/8-props/src/App.vue
@@ -0,0 +1,58 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import MyBio from "./components/Mybio.vue";
+</script>
+
+<template>
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
+
+    <div class="wrapper">
+      <HelloWorld msg="Prop's Class" />
+    </div>
+  </header>
+
+  <main>
+    <MyBio
+      name="Jarko"
+      age="32"
+      birthPlace="Malaga, España"
+      currentLocation="Malaga, Ehhpaña"
+    />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ section-04-vue/my-first-vue-project/src/components/vueBasics/8-props/src/components/MyBio.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1>hello</h1>
+  <h1>{{ name }}</h1>
+  <h4>{{ age }}</h4>
+  <h4>{{ birthPlace }}</h4>
+  <h4>{{ currentLocation }}</h4>
+</template>
+
+<script setup>
+import { ref, reactive } from "vue";
+
+// Syntax inicial :)
+//defineProps({})
+
+defineProps({
+  name: {
+    type: String,
+    required: false,
+    default: "Some Random Name",
+  },
+  age: {
+    type: Number,
+    default: 0,
+  },
+  birthPlace: {
+    type: String,
+  },
+  currentLocation: {
+    type: String,
+    required: true,
+    default: "Barcelona, España",
+  },
+});
+</script>
+
+<style></style>
