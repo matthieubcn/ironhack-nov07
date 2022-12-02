cfab2028b1e51c5c0762dfa0e96fbc3537e167f7
Diego Zito <diegozito.m@gmail.com>
2022-12-02 12:53:28 +0100

    events class sfc added

---
+++ section-04-vue/my-first-vue-project/src/components/vueBasics/11-Events.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from "vue";
+</script>
+
+<template>
+  <div>
+    <!-- Una situación muy común cuando se monta una aplicación es que querremos ser capaces de ejecutar JavaScript después de un evento en el DOM (click, submit, etc.). -->
+    <!-- Hay dos tipos de manejadores de eventos (handlers), los inline y los methods. -->
+  </div>
+</template>
